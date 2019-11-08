d05964174428ea9bacb66a22269cfd4e9fac383d
Samuel Brenner <sdbren@uw.edu>
2019-11-08 15:55:22 -0800

    updated help and renamed vect_wavenum->vectWavenum

--- Surface_Gravity_Waves/vectWavenum.m
+++ Surface_Gravity_Waves/vectWavenum.m
@@ -1,28 +1,27 @@
-function [k,err] = vect_wavenum(omega,depth,tolerance,num_iterations)
-% Linear dispersion relation wavenumber solver appropriate for arrays of 
-% 'omega' and 'depth'.
+function [k,err] = vectWavenum(omega,depth,tolerance,num_iterations)
+% VECTWAVENUM Linear dispersion relation wavenumber solver
 % 
-% k = vect_wavenum(omega,depth) solves for the wave-numbers 'k' for a given
-% depth that satisfy the dispersion relation:
-%           omega^2 = k*tanh(k*depth) 
-% using the Newton-Raphson method.
-%
-% k = vect_wavenum(omega,depth,tolerance) assigns a tolerance value for
-% this calculation. 
-% If 'tolerance' is omitted, or entered as [], then a default value of 1e-6 is used.  
-% The solver continues iterating until all values of 'k' meet this
-% tolerance level or until it reaches the maximum number of iterations.
-%
-% k = vect_wavenum(omega,depth,tolerance,num_iterations) computes 'k'
-% within a given tolerance using a maximum number of interations:
-% 'num_iterations'. 
-% If 'num_iterations' is omitted, or entered as [], then a default value of 10 is used.  
-%
-% [k,err] = vect_wavenum(omega,depth,...) returns the approximation error 
-% associated with each value 'k':
-% err = abs( k*tanh(k*d) - omega^2 )
-
-% Samuel Brenner, April 2018
+%	k = vectWavenum(omega,depth) solves for the wave-numbers 'k' for a
+%	given depth that satisfy the dispersion relation:
+%   	omega^2 = k*tanh(k*depth)
+%	using the Newton-Raphson method.  The variables omega and depth can be
+%	arrays.
+% 
+%	k = vectWavenum(omega,depth,tolerance) assigns a tolerance value for
+%	this calculation. If 'tolerance' is omitted, or entered as [], then a
+%	default value of 1e-6 is used.  The solver continues iterating until
+%	all values of 'k' meet this tolerance level or until it reaches the
+%	maximum number of iterations.
+% 
+%	k = vectWavenum(omega,depth,tolerance,num_iterations) computes 'k'
+%   within a given tolerance using a maximum number of interations:
+%   'num_iterations'. If 'num_iterations' is omitted, or entered as [],
+%   then a default value of 10 is used.
+% 
+%	[k,err] = vectWavenum(omega,depth,...) returns the approximation error
+%   associated with each value 'k': err = abs( k*tanh(k*d) - omega^2 )
+% 
+%	S.D.Brenner, April 2018
 
 %% Error checking:
 narginchk(2,4);
@@ -53,8 +52,8 @@ end
 tol = args{1};
 num_iter = args{2};
 
-% If omega or depth are scalars, they should be converted to vectors of
-% equal size:
+% If one of omega or depth are scalars, they should be converted to vectors
+% of equal size:
 if length(omega(:)) == 1
     omega = omega* ones(size(depth));
 end
@@ -62,19 +61,26 @@ if length(depth(:)) == 1
     depth = depth* ones(size(omega));
 end
 
-% Initial guess from CEM II-1-11:
+
+%% Initial guess:
+
+% from CEM II-1-11:
 % kn = (w.^2/g) .* ( tanh( (w.^2/g).*d ) ).^(-1/2)
 
-%% Initial guess from Fenton and McKee, 1990:
+% from Fenton and McKee, 1990:
 k0 = omega.^2/g; % deep water wavenumber
 kn = k0 .* ( tanh((k0.*depth).^(3/4)) ).^(-2/3);
-err = abs( omega.^2 - g*kn.*tanh(kn.*depth) ); % error for initial guess
+
+% Error for initial guess
+err = abs( omega.^2 - g*kn.*tanh(kn.*depth) ); 
 
 %% Use Newton's method: k_(n+1) = k_n + f(k_n)/f'(k_n)
+
 converged = 0; % flag to identify if the iteration has converged with tolerance
 for n = 1:num_iter
     
-    idx = find(err > tol); % on each loop, re-compute only the k's that don't meet the tolerance
+    % on each loop, re-compute only the k's that don't meet the tolerance
+    idx = find(err > tol); 
     
     fkn = g*kn(idx).*tanh(kn(idx).*depth(idx)) - omega(idx).^2;
     dfkn = g*tanh(kn(idx).*depth(idx)) + g*depth(idx).*kn(idx).*sech(kn(idx).*depth(idx)).^2;
@@ -84,7 +90,6 @@ for n = 1:num_iter
     err = abs( omega.^2 - g*kn.*tanh(kn.*depth) );
     
     % Tolerance check
-    idx = find(err > tol);
     if max(err(:)) <= tol 
         converged = 1;
         break;
