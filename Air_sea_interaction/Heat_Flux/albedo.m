2c0feeec66cc3b039724b9960254e3531f0afafe
Samuel Brenner <sdbren@uw.edu>
2019-11-09 10:19:22 -0800

    updated help and added input parsing for albedo function

--- Air_sea_interaction/Heat_Flux/albedo.m
+++ Air_sea_interaction/Heat_Flux/albedo.m
@@ -1,18 +1,24 @@
 function alpha = albedo(mu)
-% alpha = ALBEDO(mu) finds the ocean surface albedo parameterized as a 
-%   function of solar zenith angle.
+% ALBEDO finds the ocean surface albedo parameterized as a 
+% function of solar zenith angle
 %
+%   alpha = ALBEDO(mu) returns the ocean surface albedo, 'alpha', where
 %   'mu' is the cosine of the local solar zenith angle.
 %
-%   from: 
+%   Formulation from: 
 %   "Comparison of Regional Clear-Sky Albedos Inferred from Satellite  
 %   Observations and Model Computations",
 %   B. P. Briegleb et. al., 1986
 %   doi:  10.1175/1520-0450(1986)025<0214:CORCSA>2.0.CO;2
 %
-%   Samuel Brenner, 2018
+%   S.D.Brenner, 2018
 
+%% Parse input
+if ~isnumeric(mu) || abs(mu)>1
+    error('The input mu must be a numeric value between -1 and 1 (the cosine of the solar zenith angle)');
+end
 
+%% Calculate
 
 alpha = 2.6./(mu.^1.7 + 0.065) + 15.0*(mu-1).*(mu-0.5).*(mu-1.0);
 alpha = real(alpha)/100;
