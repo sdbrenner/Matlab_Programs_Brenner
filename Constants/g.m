04df29d38e781ac7dd575be78888b22599fa43c8
Samuel Brenner <sdbren@uw.edu>
2019-11-08 16:59:47 -0800

    updated help and comments (related to unit specification)

--- Constants/g.m
+++ Constants/g.m
@@ -4,14 +4,22 @@ function grav_const = g(L,T)
 %
 %   g = 9.81 m/s/s
 %
-%   g(L,T) can return g in other units
+%   g(L,T) can return g in other length/time units consistent with a
+%   'symunit' collection.  L are the units associated with length (default
+%   'm'), and T are the units associated with time (default 's');
+%
+%   S.D.Brenner, 2019
 
+% Default units:
 su = symunit;
-if nargin <2; L = 'm'; T = 's'; end
+if nargin < 2 || isempty(T); T = 's'; end
+if nargin < 1 || isempty(L); L = 'm'; end
 
-
+% Unit conversion factor (if other units are requested)
 unitConv = unitConversionFactor( su.m/su.s/su.s,...
-                                          su.(L)/su.(T)/su.(T)   );                                     
+                                  su.(L)/su.(T)/su.(T)   );
+
+% Value                                          
 grav_const = 9.81 * double(unitConv);
 
 
